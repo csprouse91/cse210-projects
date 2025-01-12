bb0ef0db2cec65b77dbbef545d76a974349a6461
csprouse91 <csprouse2010@live.com>
2025-01-11 20:56:43 -0700

    Completed Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please enter your grade percentage: ");
+        string userInputGrade = Console.ReadLine();
+        int grade = int.Parse(userInputGrade);
+        string gradeLetter = "";
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+        Console.WriteLine($"Your grade is {gradeLetter}.");
     }
 }
