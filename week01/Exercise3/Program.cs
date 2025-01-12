e333023314bf0bd93d5baa546bc3050ebb62c4bc
csprouse91 <csprouse2010@live.com>
2025-01-11 22:03:44 -0700

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Step 1 Ask user for a magic number and convert to integer for conditions
+        // Console.Write("What is the magic number? ");
+        // string userInputNumber = Console.ReadLine();
+        // int number = int.Parse(userInputNumber);
+
+        // Step 2 Ask user for a guess and convert to integer for conditions
+        // Console.Write("What is your guess? ");
+        // string userInputGuess = Console.ReadLine();
+        // int guess = int.Parse(userInputGuess);
+        // while (guess != number)
+        // {
+        //     if (guess < number)
+        //     {
+        //         Console.WriteLine("Higher");
+        //     }
+        //     else if (guess > number)
+        //     {
+        //         Console.WriteLine("Lower");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("You guessed the magic number!");
+        //     }
+        // }
+
+        // Step 3 Generate a random number between 1 and 100
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        // Try a do-while loop and ask for a guess as integer(instead of string then converting to integer)
+        int guess;
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+        } while (guess != number);
     }
 }
