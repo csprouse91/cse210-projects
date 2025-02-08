aed19cc8886a69770ca0f7bfe3e0d62c7ff8563b
csprouse91 <csprouse2010@live.com>
2025-02-07 21:39:26 -0700

    7 hours

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+//Added 
+
 class Program
 {
     static void Main(string[] args)
@@ -12,22 +14,38 @@ class Program
         Reference reference2 = new Reference("D & C", 82, 9, 10);
         Scripture scripture2 = new Scripture(reference2, "Or in other words, I give unto you directions how you may act before me, that it may turn to you for your salvation. I, the Lord, am bound when ye do what I say; but when ye do not what I say, ye have no promise.");
         //Clear the console screen and display the scripture reference and text
-        Console.Clear();
-        Console.WriteLine(scripture1.DisplayText());
-        //Prompt user to press enter or type quit
-        Console.WriteLine("Press enter to hide a word, or a number and enter to hide that number of words, or 'quit' to exit the program.");
-        //If user types quit, exit the program
-        if (Console.ReadLine().ToLower() == "quit")
+        do
         {
-            return;
-        }
-        //If user presses enter, hide a random word
-        if (Console.ReadLine() == "")
-        {
-            scripture1.HideWords(1);
-            Console.Clear();
             Console.WriteLine(scripture1.DisplayText());
-        }
+            //Prompt user to press enter or type quit
+            Console.WriteLine("Press enter to hide a word, or a number and enter to hide that number of words, or 'quit' to exit the program.");
+            string userInput = Console.ReadLine();
+            //If user types quit, exit the program
+            if (userInput.ToLower() == "quit")
+            {
+                return;
+            }
+            //If user presses enter, hide a random word
+            else if (userInput == "")
+            {
+                scripture1.HideWords(1);
+                Console.Clear();
+            }
+            //If user types a number and presses enter, hide that number of words
+            else try
+                {
+                    int wordsToHide = int.Parse(userInput);
+                    scripture1.HideWords(wordsToHide);
+                    Console.Clear();
+                }
+                catch (FormatException)//If the user types a non-integer, display this error message
+                {
+                    Console.Clear();//Clear console before displaying error message
+                    Console.WriteLine("---------------------Invalid input---------------------");
+                }
+        } while (scripture1.AllWordsHidden() == false);
+
+
         //If user types a number and presses enter, hide that number of words
         //Clear the console and display the scripture reference and text with hidden words (same length as original word with underscores)
 
