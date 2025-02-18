fa5159050f5498bd553067b3c7373f2e9b578ec3
csprouse91 <csprouse2010@live.com>
2025-02-18 13:02:21 -0700

    4 hour progress

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 
 public class GoalManager
 {
-    private List<Goal> _goals = new List<Goal>();
-    private int _score = 0;
+    private List<Goal> _goals;
+    private int _score;
 
     public GoalManager()
     {
-        
+        _goals = new List<Goal>();
+        _score = 0;
     }
 
     public void Start()
@@ -28,7 +29,41 @@ public class GoalManager
 
     public void CreateGoal()
     {
-
+        Console.WriteLine("There are three types of goals: ");
+        Console.WriteLine("1. Simple Goal");
+        Console.WriteLine("2. Eternal Goal");
+        Console.WriteLine("3. Checklist Goal");
+        Console.Write("Please indicate the type of goal you want to create: ");
+        string goalType = Console.ReadLine();
+        Console.Write("What do you want to name your goal?");
+        string goalName = Console.ReadLine();
+        Console.Write("Give a short description of your goal: ");
+        string goalDescription = Console.ReadLine();
+        Console.Write("How many points is this goal worth?");
+        string goalPoints = Console.ReadLine();
+        if (goalType == "1")
+        {
+            Goal newGoal = new SimpleGoal(goalName, goalDescription, goalPoints);
+            _goals.Add(newGoal);
+        }
+        else if (goalType == "2")
+        {
+            Goal newGoal = new EternalGoal(goalName, goalDescription, goalPoints);
+            _goals.Add(newGoal);
+        }
+        else if (goalType == "3")
+        {
+            Console.Write("How many times do you want to do this to complete this goal?");
+            int target = int.Parse(Console.ReadLine());
+            Console.Write("How many bonus points do you want this to be?");
+            int bonus = int.Parse(Console.ReadLine());
+            Goal newGoal = new ChecklistGoal(goalName, goalDescription, goalPoints, target, bonus);
+            _goals.Add(newGoal);
+        }
+        else
+        {
+            Console.WriteLine("Invalid goal type");
+        }
     }
 
     public void RecordEvent()
--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -7,6 +7,7 @@ class Program
         //Create a menu
         Console.WriteLine("Welcome to Eternal Quest!");
         string userInput;
+        GoalManager goalManager = new GoalManager();
         do
         {
             Console.WriteLine("Menu Options: ");
@@ -16,27 +17,33 @@ class Program
             Console.WriteLine("4. Load Goals");
             Console.WriteLine("5. Record Event");
             Console.WriteLine("6. Exit");
-            Console.WriteLine("Please enter one of the options: ");
+            Console.Write("Please enter one of the options: ");
             userInput = Console.ReadLine();
             switch (userInput)
             {
                 case "1":
                     Console.WriteLine("Creating a new goal");
+                    goalManager.CreateGoal();
                     break;
                 case "2":
                     Console.WriteLine("Listing goals");
+                    goalManager.ListGoalNames();
                     break;
                 case "3":
                     Console.WriteLine("Saving goals");
+                    goalManager.SaveGoals();
                     break;
                 case "4":
                     Console.WriteLine("Loading goals");
+                    goalManager.LoadGoals();
                     break;
                 case "5":
                     Console.WriteLine("Recording event");
+                    goalManager.RecordEvent();
                     break;
                 case "6":
                     Console.WriteLine("Exiting");
+                    Environment.Exit(0);
                     break;
                 default:
                     Console.WriteLine("Invalid option");
